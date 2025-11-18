f73f6de359b5e7ca5d0d67f0ef1e27f0cc38c8f4
zaporter-work <work-github@zackporter.com>
2025-11-17 19:03:42 -0800

    diffs

--- primes.sh
+++ primes.sh
@@ -1,10 +1,10 @@
 
 run_name="pretrain_att_primes"
-python pretrain.py \
+torchrun --nproc-per-node 4 --rdzv_backend=c10d --rdzv_endpoint=localhost:0 --nnodes=1 pretrain.py \
 arch=trm \
 data_paths="[data/factorization-test]" \
 evaluators="[]" \
-epochs=5000 eval_interval=5000 \
+epochs=50000 eval_interval=5000 \
 lr=1e-4 puzzle_emb_lr=1e-4 weight_decay=1.0 puzzle_emb_weight_decay=1.0 \
 arch.L_layers=2 \
 arch.H_cycles=3 arch.L_cycles=6 \
--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 uv venv
 source .venv/bin/activate
 uv pip install --upgrade pip wheel setuptools
-uv pip install --pre --upgrade torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu129
+uv pip install --pre --upgrade torch torchvision torchaudio --index-url https://download.pytorch.org/whl/nightly/cu128
 uv pip install -r requirements.txt # install requirements
 uv pip install --no-cache-dir --no-build-isolation adam-atan2-pytorch
